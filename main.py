1b3e765967c7bc7575e6afc25fbc33f0256c4a09
KubaBoi <jakubanderle@outlook.cz>
2020-12-06 22:54:41 +0100

    debug

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ camera.rotation = 180
 
 print("Starting...")
 while True:
-    print("Taking picture...")
-    camera.capture('image.png')
+    try:
+        print("Taking picture...")
+        camera.capture('image.png')
 
-    print("Posting picture...")
-    url = "http://kuba-test.borec.cz/cameraServer/upload.php"
-    files = {'file': open('image.png', 'rb')}
-    r = requests.post(url, files=files)
+        print("Posting picture...")
+        url = "http://kuba-test.borec.cz/cameraServer/upload.php"
+        files = {'file': open('image.png', 'rb')}
+        r = requests.post(url, files=files)
 
-    print(r.text)
+        print(r.text)
+    except Exception as e:
+        print(e)
 
     sleep(5)
