42a6c42e2fe7ed48b93efd88b9fd7af98725b781
KubaBoi <jakubanderle@outlook.cz>
2020-12-07 01:31:25 +0100

    debug

--- upload.php
+++ upload.php
@@ -1,7 +1,10 @@
 <?php
+$save = $_REQUEST["save"];
+
 $fileName = "gallery/image" . date("Ymdgi") . ".png";
 print_r($_FILES);
-move_uploaded_file($_FILES["file"]["tmp_name"],$fileName);
+if ($save == "1"):
+    move_uploaded_file($_FILES["file"]["tmp_name"],$fileName);
 move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
 
 echo $_FILES["file"]["tmp_name"] . "\n";
