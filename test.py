6ea8936aae8c261e2b346ffc7f7c5708fca5bbcb
KubaBoi <jakubanderle@outlook.cz>
2020-12-07 02:09:06 +0100

    debug

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://kuba-test.borec.cz/cameraServer/upload.php"
+url = "http://kuba-test.borec.cz/cameraServer/upload.php?save=1"
 files = {'file': open('image.png', 'rb')}
 r = requests.post(url, files=files)
 
--- upload.php
+++ upload.php
@@ -1,14 +1,28 @@
 <?php
-$save = $_REQUEST["save"];
+$uri = $_SERVER['REQUEST_URI'];
+echo "\n";
+echo $uri . "\n";
+$save = explode("=", $uri )[1];
 
 $fileName = "gallery/image" . date("Ymdgi") . ".png";
 print_r($_FILES);
-if ($save == "1"):
-    move_uploaded_file($_FILES["file"]["tmp_name"],$fileName);
-move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
+move_uploaded_file($_FILES["file"]["tmp_name"], $_FILES["file"]["name"]);
+
+if ($save == 1) {
+    echo "Saving\n";
+    move_uploaded_file($_FILES["file"]["tmp_name"], $fileName);
+    if (file_exists($fileName)) {
+        echo "Saved\n";
+    }
+} else {
+    echo "Not Saving\n";
+}
+
+print_r(scandir("gallery"));
 
 echo $_FILES["file"]["tmp_name"] . "\n";
 echo $_FILES["file"]["name"] . "\n";
+echo $save . "\n";
 echo $fileName;
 
 ?>
